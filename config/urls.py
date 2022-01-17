a3d990c69fb979eb41ba4925a48dd4e30913643e
Stanislav Chm <stanislavchmlv@gmail.com>
2022-01-17 23:30:23 +0300

    hncustomnewsletters 2.0 | Initial project setup

---
+++ config/urls.py
@@ -0,0 +1,29 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import environ
+import os
+
+from config.settings import BASE_DIR
+from django.contrib import admin
+from django.urls import path, include
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+urlpatterns = [
+    path(env('ADMIN_URL'), admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
+]
