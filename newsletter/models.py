1961ff9b36519312a67d0d5974faabefff8f8301
Stanislav Chm <stanislavchmlv@gmail.com>
2022-05-16 00:09:14 +0300

    add verbose_name to fields

--- newsletter/models.py
+++ newsletter/models.py
@@ -14,11 +14,13 @@ class Thread(TimeStampedModel, models.Model):
     """Parsed thread (story) from hackernews"""
 
     thread_id = models.PositiveBigIntegerField(unique=True, help_text="hackernews thread id")
-    link = models.URLField(max_length=700)
-    title = models.CharField(max_length=100)
-    score = models.IntegerField(null=True)
-    comments_count = models.IntegerField(null=True)
-    comments_link = models.URLField(max_length=250, null=True)
+    link = models.URLField(max_length=700, verbose_name="story link")
+    title = models.CharField(max_length=100, verbose_name="thread title")
+    score = models.IntegerField(null=True, verbose_name="thread score")
+    comments_count = models.IntegerField(null=True, verbose_name="thread comments count")
+    comments_link = models.URLField(
+        max_length=250, null=True, verbose_name="link to thread comments"
+    )
 
     class LastWeekThreads(models.Manager):
         def get_queryset(self):
@@ -57,24 +59,24 @@ class Newsletter(TimeStampedModel, models.Model):
         FIVE = 5
         TEN = 10
 
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    verified = models.BooleanField(default=False)
+    name = models.CharField(max_length=200, verbose_name="newsletter name")
+    email = models.EmailField(verbose_name="email subscribed to this newsletter")
+    verified = models.BooleanField(default=False, verbose_name="email verified")
     weekday = models.CharField(
         max_length=50,
         choices=NewsletterWeekdayChoices.choices,
         default=NewsletterWeekdayChoices.THURSDAY,
-        help_text="""The weekday on which the newsletter will be sent""",
+        help_text="""the weekday on which the newsletter will be sent""",
     )
     min_thread_score = models.PositiveSmallIntegerField()
     max_threads_count = models.PositiveSmallIntegerField(
         choices=NewsletterThreadsCountChoices.choices,
         default=NewsletterThreadsCountChoices.FIVE,
-        help_text="Max number of threads per single tag",
+        help_text="max number of threads per single tag",
     )
     slug = models.SlugField(max_length=250)
-    tags = ArrayField(models.CharField(max_length=80))
-    domens = ArrayField(models.CharField(max_length=80))
+    tags = ArrayField(models.CharField(max_length=80), verbose_name="newsletter tags")
+    domens = ArrayField(models.CharField(max_length=80), verbose_name="newsletter domens")
 
     def __str__(self):
         return f"({self.pk}) {self.name}"
