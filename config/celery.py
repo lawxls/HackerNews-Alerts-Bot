808c8d44a38a7e4d0555aa48c711d49399c0c316
Stanislav Chm <stanislavchmlv@gmail.com>
2022-05-28 01:22:02 +0300

    beat_schedule, thread_scraper_cron_task

--- config/celery.py
+++ config/celery.py
@@ -1,12 +1,20 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "thread_scraper_cron_task": {
+        "task": "scraper.tasks.thread_scraper_cron_task",
+        "schedule": crontab(minute="*/2"),
+    }
+}
 
 
 @app.task(bind=True)
--- scraper/tasks.py
+++ scraper/tasks.py
@@ -1,6 +1,10 @@
 from config import celery_app
+from scraper.scrape_threads import ThreadScraper
 
 
 @celery_app.task
-def add(x, y):
-    return x + y
+def thread_scraper_cron_task():
+    """scrape threads every 2 minutes"""
+
+    thread_scraper = ThreadScraper()
+    thread_scraper.scrape_pages_and_save_threads()
