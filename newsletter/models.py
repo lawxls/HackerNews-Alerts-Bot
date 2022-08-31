87e14fe70fae2d41b8e776a47f28b2089d7cd5f2
Stanislav Chm <stanislavchmlv@gmail.com>
2022-09-01 01:02:49 +0300

    remake Newsletter model, delete migrations

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,18 +1,15 @@
-import logging
-
 from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.template.defaultfilters import slugify
 from model_utils.models import TimeStampedModel
 
-logging.basicConfig(level="DEBUG")
-
 
 class Newsletter(TimeStampedModel, models.Model):
-    """Customizable newsletter"""
+    """Customizable email newsletter"""
 
     class NewsletterWeekdayChoices(models.TextChoices):
-        """The weekday on which the newsletter will be sent"""
+        """Send newsletter on selected day of the week"""
 
         MONDAY = "MONDAY"
         TUESDAY = "TUESDAY"
@@ -22,42 +19,27 @@ class Newsletter(TimeStampedModel, models.Model):
         SATURDAY = "SATURDAY"
         SUNDAY = "SUNDAY"
 
-    class NewsletterThreadsCountChoices(models.TextChoices):
-        """Max number of threads per single tag"""
-
-        THREE = 3
-        FIVE = 5
-        TEN = 10
-
     name = models.CharField(max_length=200, verbose_name="newsletter name")
     weekday = models.CharField(
         max_length=50,
         choices=NewsletterWeekdayChoices.choices,
         default=NewsletterWeekdayChoices.THURSDAY,
-        help_text="""the weekday on which the newsletter will be sent""",
+        verbose_name="send newsletter on set weekday",
     )
-    min_thread_score = models.PositiveSmallIntegerField()
-    max_threads_count = models.PositiveSmallIntegerField(
-        choices=NewsletterThreadsCountChoices.choices,
-        default=NewsletterThreadsCountChoices.FIVE,
-        help_text="max number of threads per single tag",
-    )
-    tags = ArrayField(models.CharField(max_length=80), verbose_name="newsletter tags")
-    domens = ArrayField(models.CharField(max_length=80), verbose_name="newsletter domens")
-
-    def __str__(self):
-        return f"({self.pk}) {self.name}"
-
-    class Meta:
-        ordering = ["-created"]
-
-
-class EmailNewsletter(Newsletter):
-    """email newsletter"""
-
-    email = models.EmailField(verbose_name="email subscribed to this newsletter")
-    verified = models.BooleanField(default=False, verbose_name="email verified")
+    email = models.EmailField(verbose_name="subscribed email")
     slug = models.SlugField(max_length=250)
+    keywords = ArrayField(models.CharField(max_length=80), verbose_name="newsletter keywords")
+    domains = ArrayField(models.CharField(max_length=80), verbose_name="newsletter domains")
+    score_threshold = models.PositiveSmallIntegerField(
+        verbose_name="filter out stories below set score threshold",
+        default=10,
+        validators=[MaxValueValidator(1000)],
+    )
+    stories_per_keyword_max_count = models.PositiveSmallIntegerField(
+        verbose_name="max number of stories per keyword",
+        validators=[MaxValueValidator(10), MinValueValidator(3)],
+    )
+    verified = models.BooleanField(default=False, verbose_name="is email verified")
 
     def __str__(self):
         return f"({self.pk}) {self.name}"
@@ -65,18 +47,6 @@ class EmailNewsletter(Newsletter):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
-
-    class Meta:
-        ordering = ["-created"]
-
-
-class TelegramNewsletter(Newsletter):
-    """telegram newsletter"""
-
-    user_id = models.PositiveIntegerField(verbose_name="telegram user id")
-
-    def __str__(self):
-        return f"({self.pk}) {self.name}"
 
     class Meta:
         ordering = ["-created"]
