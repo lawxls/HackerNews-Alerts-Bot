80febc69e54f136f6f2acd50da80dc047fc0a14e
Stanislav Chm <stanislavchmlv@gmail.com>
2022-09-29 03:04:42 +0300

    GetUpdates method test

---
+++ telegram_feed/tests/test_get_updates_request.py
@@ -0,0 +1,11 @@
+import pytest
+
+from telegram_feed.requests import GetUpdates
+
+
+class TestGetUpdates:
+    @pytest.mark.django_db
+    def test_get_updates_method(self):
+        update_objs = GetUpdates().get_updates()
+
+        assert isinstance(update_objs, list)
