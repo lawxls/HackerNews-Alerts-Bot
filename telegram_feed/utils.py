1c5754b1dac1db87ccb323561afd73366cb1d036
Stanislav Chm <stanislavchmlv@gmail.com>
2022-11-13 02:31:46 +0300

    escape_markdown utils function

--- telegram_feed/utils.py
+++ telegram_feed/utils.py
@@ -1,3 +1,4 @@
+import re
 from time import sleep
 
 from django.db.models.query import QuerySet
@@ -34,3 +35,30 @@ def send_threads_to_telegram_feed(user_feed: UserFeed, threads: QuerySet[Thread]
         )
 
     return True
+
+
+def escape_markdown(text: str, version: int = 1, entity_type: str = None) -> str:
+    """
+    Helper function to escape telegram markup symbols.
+    Args:
+        text (:obj:`str`): The text.
+        version (:obj:`int` | :obj:`str`): Use to specify the version of telegrams Markdown.
+            Either ``1`` or ``2``. Defaults to ``1``.
+        entity_type (:obj:`str`, optional): For the entity types ``PRE``, ``CODE`` and the link
+            part of ``TEXT_LINKS``, only certain characters need to be escaped in ``MarkdownV2``.
+            See the official API documentation for details. Only valid in combination with
+            ``version=2``, will be ignored else.
+    """
+    if version == 1:
+        escape_chars = r"_*`["
+    elif version == 2:
+        if entity_type in {"pre", "code"}:
+            escape_chars = r"\`"
+        elif entity_type == "text_link":
+            escape_chars = r"\)"
+        else:
+            escape_chars = r"_*[]()~`>#+-=|{}.!"
+    else:
+        raise ValueError("Markdown version must be either 1 or 2!")
+
+    return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
