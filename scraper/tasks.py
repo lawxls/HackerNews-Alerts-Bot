6479ccdaf9ecef8a3971f37bc2412c96a5c7cea9
Stanislav Chm <stanislavchmlv@gmail.com>
2022-11-13 12:36:51 +0300

    return amount of threads parsed in scraper tasks

--- scraper/tasks.py
+++ scraper/tasks.py
@@ -3,16 +3,18 @@ from scraper.service import ThreadScraper
 
 
 @celery_app.task
-def new_threads_scraper_cron_task():
+def new_threads_scraper_cron_task() -> int:
     """scrape threads from /newest page"""
 
     newest_page_thread_scraper = ThreadScraper()
-    newest_page_thread_scraper.scrape()
+    threads = newest_page_thread_scraper.scrape()
+    return len(threads)
 
 
 @celery_app.task
-def main_page_threads_scraper_cron_task():
+def main_page_threads_scraper_cron_task() -> int:
     """scrape threads from /news page"""
 
     main_page_thread_scraper = ThreadScraper(page_to_scrape=ThreadScraper.NEWS, news_page_count=10)
-    main_page_thread_scraper.scrape()
+    threads = main_page_thread_scraper.scrape()
+    return len(threads)
