d82d72f0660490586c2e943e21661f706e17caba
Stanislav Chm <stanislavchmlv@gmail.com>
2022-12-02 03:37:53 +0300

    fix score_threshold & score inconsistency in task tests

--- telegram_feed/tests/test_tasks.py
+++ telegram_feed/tests/test_tasks.py
@@ -17,10 +17,12 @@ class TestSendStoriesToUserChatsTask:
 
         user_feed = UserFeedFactory.create(keywords=["python", "django", "tomato"])
 
-        thread1 = ThreadFactory.create(title="test title with Python keyword")
-        thread2 = ThreadFactory.create(title="test title with Django keyword")
-        thread3 = ThreadFactory.create(title="test title with tomato keyword")
-        thread4 = ThreadFactory.create(title="test title with Cucumber non-existing keyword")
+        thread1 = ThreadFactory.create(title="test title with Python keyword", score=100)
+        thread2 = ThreadFactory.create(title="test title with Django keyword", score=100)
+        thread3 = ThreadFactory.create(title="test title with tomato keyword", score=100)
+        thread4 = ThreadFactory.create(
+            title="test title with Cucumber non-existing keyword", score=100
+        )
 
         send_stories_to_user_chats_task()
 
@@ -53,17 +55,13 @@ class TestSendStoriesToUserChatsTask:
     def test_sending_stories_from_past_24_hours(self, send_message_mock):
         send_message_mock.return_value = True
 
-        user_feed = UserFeedFactory.create(
-            keywords=["python"],
-        )
+        user_feed = UserFeedFactory.create(keywords=["python"])
 
         date_from = timezone.now() - datetime.timedelta(days=2)
         thread1 = ThreadFactory.create(
-            title="test title with Python keyword 2 days old", created=date_from
+            title="test title with Python keyword 2 days old", score=100, created=date_from
         )
-        thread2 = ThreadFactory.create(
-            title="test title with Python keyword new", created=timezone.now()
-        )
+        thread2 = ThreadFactory.create(title="test title with Python keyword new", score=100)
 
         send_stories_to_user_chats_task()
 
@@ -75,16 +73,12 @@ class TestSendStoriesToUserChatsTask:
     def test_sending_stories_to_multiple_user_feeds(self, send_message_mock):
         send_message_mock.return_value = True
 
-        user_feed1 = UserFeedFactory.create(
-            keywords=["python", "django"],
-        )
-        user_feed2 = UserFeedFactory.create(
-            keywords=["python", "c++"],
-        )
+        user_feed1 = UserFeedFactory.create(keywords=["python", "django"])
+        user_feed2 = UserFeedFactory.create(keywords=["python", "c++"])
 
-        thread1 = ThreadFactory.create(title="test title with Python keyword")
-        thread2 = ThreadFactory.create(title="test title with c++ keyword")
-        thread3 = ThreadFactory.create(title="test title with django keyword")
+        thread1 = ThreadFactory.create(title="test title with Python keyword", score=100)
+        thread2 = ThreadFactory.create(title="test title with c++ keyword", score=100)
+        thread3 = ThreadFactory.create(title="test title with django keyword", score=100)
 
         send_stories_to_user_chats_task()
 
