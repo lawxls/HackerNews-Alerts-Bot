bfa079f1252b400a8f9f7c8f7fa090aaa268aa0e
Stanislav Chm <stanislavchmlv@gmail.com>
2022-12-03 04:14:03 +0300

    Keyword model, UserFeed: comments ManyToMany field

--- telegram_feed/models.py
+++ telegram_feed/models.py
@@ -1,22 +1,23 @@
 from django.contrib.postgres.fields import ArrayField
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.db import models
 from model_utils.models import TimeStampedModel
 
-from scraper.models import Thread
+from scraper.models import Comment, Thread
 
 
 class UserFeed(TimeStampedModel, models.Model):
     """Telegram user feed"""
 
     chat_id = models.PositiveIntegerField(verbose_name="telegram chat id")
-    keywords = ArrayField(models.CharField(max_length=80), verbose_name="feed keywords")
+    old_keywords = ArrayField(models.CharField(max_length=80), verbose_name="feed keywords")
     score_threshold = models.PositiveSmallIntegerField(
         verbose_name="Threshold to pass for a story to be sent",
         default=1,
         validators=[MaxValueValidator(10000)],
     )
     threads = models.ManyToManyField(Thread, related_name="user_feeds")
+    comments = models.ManyToManyField(Comment, related_name="user_feeds")
 
     def __str__(self):
         return f"({self.pk}) {self.chat_id}"
@@ -35,6 +36,28 @@ class TelegramUpdate(TimeStampedModel, models.Model):
 
     def __str__(self):
         return f"({self.pk}) {self.update_id}"
+
+    class Meta:
+        ordering = ["-created"]
+
+
+class Keyword(TimeStampedModel, models.Model):
+    """Keyword to search for and it's data"""
+
+    user_feed = models.ForeignKey(UserFeed, on_delete=models.CASCADE, related_name="keywords")
+    name = models.CharField(
+        max_length=100, validators=[MinLengthValidator(2)], verbose_name="keyword itself"
+    )
+    is_full_match = models.BooleanField(default=False, verbose_name="search a whole word match")
+    search_threads = models.BooleanField(
+        default=True, verbose_name="search in thread's title field"
+    )
+    search_comments = models.BooleanField(
+        default=True, verbose_name="search in comment's body field"
+    )
+
+    def __str__(self):
+        return f"({self.pk}) {self.name}"
 
     class Meta:
         ordering = ["-created"]
