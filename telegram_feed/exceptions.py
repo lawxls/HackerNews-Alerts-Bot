1008153dced8790116b3cb08e0cfb6bacb2b4e46
Stanislav Chm <stanislavchmlv@gmail.com>
2022-12-10 04:11:05 +0300

    InvalidOptionError & BadOptionCombinationError exceptions

--- telegram_feed/exceptions.py
+++ telegram_feed/exceptions.py
@@ -1,2 +1,20 @@
 class TelegramRequestError(Exception):
     """Telegram API request error"""
+
+
+class InvalidOptionError(Exception):
+    """Command option doesn't exist error"""
+
+    def __init__(self, option: str) -> None:
+        super().__init__(option)
+
+
+class BadOptionCombinationError(Exception):
+    """Command options cannot be used together error"""
+
+    def __init__(self, options: list[str]) -> None:
+        self.options = options
+        super().__init__(options)
+
+    def __iter__(self):
+        yield from self.options
