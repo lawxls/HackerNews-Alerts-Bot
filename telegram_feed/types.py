b671a50426be5c0ba71683100be4b89fe7957e4a
Stanislav Chm <stanislavchmlv@gmail.com>
2022-12-10 04:11:49 +0300

    KeywordData dataclass

--- telegram_feed/types.py
+++ telegram_feed/types.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import TypedDict
 
+from telegram_feed.models import UserFeed
+
 
 @dataclass(frozen=True)
 class UpdateData:
@@ -10,6 +12,17 @@ class UpdateData:
     chat_id: int
     unix_timestamp_date: int
     text: str
+
+
+@dataclass
+class KeywordData:
+    """Keyword data to create Keyword object from"""
+
+    user_feed: UserFeed
+    name: str
+    is_full_match: bool = False
+    search_threads: bool = True
+    search_comments: bool = True
 
 
 class InlineKeyboardButton(TypedDict):
