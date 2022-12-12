f8bb9eb418d40512711d2fc678352acd4373fe7c
Stanislav Chm <stanislavchmlv@gmail.com>
2022-12-12 23:48:23 +0300

    add indexes for title & body fields

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     # 3rd party apps
     "django_extensions",
     "debug_toolbar",
--- scraper/models.py
+++ scraper/models.py
@@ -1,4 +1,6 @@
+from django.contrib.postgres.indexes import GinIndex, OpClass
 from django.db import models
+from django.db.models.functions import Upper
 from model_utils.models import TimeStampedModel
 
 
@@ -7,7 +9,7 @@ class Thread(TimeStampedModel, models.Model):
 
     thread_id = models.PositiveBigIntegerField(unique=True, help_text="thread id")
     link = models.URLField(max_length=2000, verbose_name="story link")
-    title = models.CharField(max_length=100, verbose_name="thread title")
+    title = models.CharField(max_length=100, verbose_name="thread title", db_index=True)
     score = models.IntegerField(null=True, verbose_name="thread score")
     thread_created_at = models.DateTimeField(verbose_name="parsed thread date of creation")
     comments_count = models.IntegerField(null=True, verbose_name="thread comments count")
@@ -19,7 +21,12 @@ class Thread(TimeStampedModel, models.Model):
         return f"({self.pk}) {self.title}"
 
     class Meta:
-        ordering = ["-score"]
+        ordering = ["-created"]
+        indexes = [
+            models.Index(Upper("title"), name="title_upper_index"),
+            GinIndex(fields=["title"], name="title_gin_index", opclasses=["gin_trgm_ops"]),
+            GinIndex(OpClass(Upper("title"), name="gin_trgm_ops"), name="title_upper_gin_index"),
+        ]
 
 
 class Comment(TimeStampedModel, models.Model):
@@ -32,10 +39,15 @@ class Comment(TimeStampedModel, models.Model):
     thread_id_int = models.PositiveIntegerField(verbose_name="thread id")
     comment_created_at = models.DateTimeField(verbose_name="parsed comment date of creation")
     username = models.CharField(max_length=20, verbose_name="comment's creator username")
-    body = models.TextField(verbose_name="comment's text body")
+    body = models.TextField(verbose_name="comment's text body", db_index=True)
 
     def __str__(self):
         return f"({self.pk}) {self.body[:100]}"
 
     class Meta:
         ordering = ["-created"]
+        indexes = [
+            models.Index(Upper("body"), name="body_upper_index"),
+            GinIndex(fields=["body"], name="body_gin_index", opclasses=["gin_trgm_ops"]),
+            GinIndex(OpClass(Upper("body"), name="gin_trgm_ops"), name="body_upper_gin_index"),
+        ]
